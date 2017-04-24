46fe9366c453cffb5bb93ed7d62acdf5c7f1b7f3
maxauz <apaza.maximiliano@gmail.com>
2017-04-24 18:56:57 -0500

    Delete Banco.sql

--- PARTE02/BDdata/Banco.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Banco]
-(
-	[id] INT NOT NULL PRIMARY KEY, 
-    [Nombre] VARCHAR(150) NULL, 
-    [Direccion] VARCHAR(350) NULL, 
-    [FechaRegistro] TIMESTAMP NOT NULL DEFAULT(GETDATE())
-)
